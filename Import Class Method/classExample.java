5e6e08fa6731770fa1b4076272848d3bcb852ce0
Lucas <70538655+laPronoia@users.noreply.github.com>
2020-09-01 20:28:44 +0200

    Import Class Method

---
+++ Import Class Method/classExample.java
@@ -0,0 +1,25 @@
+package packA;
+
+
+// this package is an example for importPacked, to show how to get methods from another packed
+public class classExample {
+	
+
+		  public static void myMethod() { 
+		    System.out.println("I just got executed! myMethod");
+		  }
+		  
+		  
+		  
+		  public static void myParaMethod(String fname) {
+			    System.out.println(fname + " Refsnes");
+			  }
+		  
+		  
+		  public static int myReturnMethod(int x) {
+			    return 5 + x;
+			  }
+
+
+
+}
