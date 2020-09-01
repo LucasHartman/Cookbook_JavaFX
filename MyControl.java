2eae15fbc89cfa62f4359c2ce3a6bbc9845788ca
laPronoia <70538655+laPronoia@users.noreply.github.com>
2020-09-01 12:26:22 +0200

    Play audio file
    An example how to play an (short) audio file in javaFX

--- MyControl.java
+++ MyControl.java
@@ -1,8 +1,36 @@
-package ControllerFXML;
+package playAudio;
 
+import java.io.File;
 
+import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class MyControl {
+	
+	
+	//local
+	//String path = "/home/lucas/eclipse-workspace/sandbox/src/playerAudio/open.wav";
+	String path = "src/audio/introA.wav";
+
+	Media media = new Media(new File(path).toURI().toString());
+	
+	// url
+	//Media media = new Media("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+
+	
+	// get MediaPlayer
+	MediaPlayer mediaPlayer = new MediaPlayer(media);
+
+	
+	
+	@FXML public void initialize() {
+		
+		System.out.println("initialize");
+		
+		// play media file
+		mediaPlayer.play();
+	}
 
 }
 
--- MyMain.java
+++ MyMain.java
@@ -1,13 +1,10 @@
-package ControllerFXML;
+package playAudio;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-
-
- // Source:					https://javafxpedia.com/en/tutorial/1580/fxml-and-controllers
 
 
 public class MyMain extends Application {
@@ -19,8 +16,7 @@ public class MyMain extends Application {
 		System.out.println("javafx.version: "	+System.getProperty("javafx.version"));
 		
 		// Exception = sub class
-        Parent root = FXMLLoader.load(getClass().getResource("MyLayout.fxml"));		// Load fxml
-        //Parent root = FXMLLoader.load(getClass().getResource("PersonLayout.fxml"));		// Load fxml
+        Parent root = FXMLLoader.load(getClass().getResource("MyLayout.fxml"));
         
         
         Scene scene = new Scene(root);
